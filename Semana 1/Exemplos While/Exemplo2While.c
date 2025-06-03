1f092b1ec13e3ec45ca9e432b28caf0933511094
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-06-03 07:33:00 -0300

    Add files via upload

---
+++ Semana 1/Exemplos While/Exemplo2While.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    // Pares até 10
+    int num;
+    num = 0;
+    while (num <= 10)
+    {
+        printf("%d\n", num);
+        num = num + 2;
+    }
+    return (0);
+}
---
+++ Semana 1/Exemplos While/Exemplo3While.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int goleAgua = 0;
+
+    while (goleAgua < 5)
+    {
+        goleAgua = goleAgua + 1;
+        printf("Bebendo 1 gole de agua, total de goles atuais: %d\n", goleAgua);
+    }
+    return (0);
+}
