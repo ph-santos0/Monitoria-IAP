f1d9c3e783cfec5638cc9054f0fe903b39711835
Pedro Henrique Santos <132759436+ph-santos0@users.noreply.github.com>
2025-06-17 16:14:09 -0300

    Update Atividade 15.c

--- 02 - While, Do While e For/01 - Exercicios da Karina/Atividade 15.c
+++ 02 - While, Do While e For/01 - Exercicios da Karina/Atividade 15.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    float tf, tc;
+int main()
+{
+    float tc, tf = 50;
 
-    tf = 50.0;
-
-    printf("Conversao: Fahrenheit -> Celsius\n");
-
-    do {
+    do
+    {
         tc = (tf - 32.0) * 5.0 / 9.0;
         printf("%.1f graus Fahrenheit = %.2f graus Celsius.\n", tf, tc);
-        tf += 1.0;
-    } while (tf <= 70.0);
-
-    return 0;
+        tf++;
+    } while (tf <= 70);
 }
